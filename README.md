6c59074ea58a54e6f2a49e36c9792f872e4e4017
Anthony Shew <anthonyshew@gmail.com>
2022-01-15 11:49:01 -0700

    Seeding of firebase-emulators.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Welcome to kittr
+
+# How to Use
+
+## Create new images for development
+
+`npm run dev:build`
+
+You may also `npm run dev:build -- -d` if you don't care about seeing logs.
+
+## Use the existing image
+
+`npm run dev`
+You may also `npm run dev -- -d` if you don't care about seeing logs.
+
+## Ports
+
+- Web front-end: 3000
+- API: 5000
+
+Firebase:
+
+- UI: 4000
+- Auth: 4001
+- Storage: 4002
+
+## Developer Notes
+
+- You may set environment variables in a `.env` file at the top of the file. The example file already includes the proper firebase-emulators ports. You will need variables for everything but the Stripe stuff for now.
+- To seed the firebase-emulators, you will need to shell into the `web` docker container and use `npm run emulators:seed`.
+
+## Dev Environment Needed Improvements:
+
+- Automatic firebase-emulators seeding
+- Local mongodb instance
---
+++ web/services/firebase/auth/seedEmulators.ts
@@ -0,0 +1,62 @@
+// IMPORTANT: ONLY TO BE USED IN TESTING AND DEV ENVIRONMENTS
+// THEY CAN ONLY BE USED IN PROPERLY SET ENVIRONMENTS
+// IF YOU ARE UNSURE OF HOW TO HANDLE THESE ENDPOINTS, DO NOT TOUCH THEM
+
+const admin = require("firebase-admin")
+
+const users = {
+	anthony: {
+		uid: "w5lMLvVLL3uJNRuoqSWvYjNIJ1GF",
+		displayName: "anthony",
+		email: "anthony@dev.com",
+		password: "123123",
+		emailVerified: true
+	},
+	fog: {
+		uid: "VeoTKmMs72amUYzPRovBGJDJnGn1",
+		displayName: "fog",
+		email: "fog@dev.com",
+		password: "123123",
+		emailVerified: true
+	},
+	filod: {
+		uid: "XnRFlNNbQ4d3K6nfEIRGBJm5nER2",
+		displayName: "filod",
+		email: "filod@dev.com",
+		password: "123123",
+		emailVerified: true
+	},
+	ringberg: {
+		uid: "AnRFlNNbQ4d3K6nfEIRGBJm5nER3",
+		displayName: "ringberg",
+		email: "ringberg@dev.com",
+		password: "123123",
+		emailVerified: true
+	}
+}
+
+if (!admin.apps.length) {
+	console.log("Using admin in emulator mode.")
+	admin.initializeApp({ projectId: "dev" })
+}
+
+const auth = admin.auth()
+
+console.log("Seeding firebase emulators...")
+auth
+	.deleteUsers([
+		"w5lMLvVLL3uJNRuoqSWvYjNIJ1GF",
+		"VeoTKmMs72amUYzPRovBGJDJnGn1",
+		"XnRFlNNbQ4d3K6nfEIRGBJm5nER2",
+		"AnRFlNNbQ4d3K6nfEIRGBJm5nER3"
+	])
+	.then(async () => {
+		try {
+			await auth.createUser(users.anthony)
+			await auth.createUser(users.fog)
+			await auth.createUser(users.filod)
+			await auth.createUser(users.ringberg)
+		} catch (error) {
+			console.error(error)
+		}
+	})
