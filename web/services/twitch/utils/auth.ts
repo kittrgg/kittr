e262b22bd432b635a778660acfab4924d694451c
Alex <alex.ringberg@gmail.com>
2022-01-29 11:52:07 -0600

    Revert "Consolidating Twitch_Client_Secrets"
    This reverts commit 7c079eb1d3c44d13cd7b55d4bd21020dc4011261.

--- web/services/twitch/utils/auth.ts
+++ web/services/twitch/utils/auth.ts
@@ -2,7 +2,7 @@
 export const getAuthToken = async () => {
 	const tokenUrl = `https://id.twitch.tv/oauth2/token?client_id=${
 		process.env.TWITCH_CLIENT_ID as string
-	}&client_secret=${process.env.TWITCH_CLIENT_SECRET as string}&grant_type=client_credentials&scope=`
+	}&client_secret=${process.env.TWITCH_SECRET as string}&grant_type=client_credentials&scope=`
 
 	try {
 		const response = await fetch(tokenUrl, {
