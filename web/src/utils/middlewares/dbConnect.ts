dcb15064b16316df3dce60d96c142d805757b32b
Anthony Shew <anthonyshew@gmail.com>
2022-01-31 11:45:37 -0700

    Mongoose migration to v6.

--- web/src/utils/middlewares/dbConnect.ts
+++ web/src/utils/middlewares/dbConnect.ts
@@ -4,9 +4,7 @@ const dbMiddleWare = async (req: any, res: any, next: any) => {
 	try {
 		if (!(global as any).mongoose) {
 			;(global as any).mongoose = mongoose.connect(process.env.DB_CONNECTION_STRING as string, {
-				authSource: "admin",
-				useNewUrlParser: true,
-				useUnifiedTopology: true
+				authSource: "admin"
 			})
 		}
 	} catch (err) {
