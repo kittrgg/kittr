512dd9fd9af178da795405eecc95d483e289e727
Anthony Shew <anthonyshew@gmail.com>
2022-02-01 13:19:40 -0700

    Find out where I'm losing NODE_ENV.

--- web/src/pages/api/admin/cypress/seedDatabase.api.ts
+++ web/src/pages/api/admin/cypress/seedDatabase.api.ts
@@ -11,6 +11,7 @@ import premiumChannelFixture from "@Utils/fixtures/mongoose/premiumChannel.json"
 const handler = createHandler()
 
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+	console.log(process.env.NODE_ENV)
 	if (process.env.NODE_ENV === "test") {
 		const rawChannel = channelFixture
 		const rawPremiumChannel = premiumChannelFixture
