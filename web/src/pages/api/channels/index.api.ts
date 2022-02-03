5257baca8cece828a16998b65bd4437039f3d1e8
Anthony Shew <anthonyshew@gmail.com>
2022-02-03 15:46:58 -0700

    Fix: Types for Channel model.

--- web/src/pages/api/channels/index.api.ts
+++ web/src/pages/api/channels/index.api.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 import type { NextApiRequest, NextApiResponse } from "next"
 import { createHandler } from "@Middlewares/createHandler"
-import Channel from "@Services/mongodb/models/Channel"
+import Channel, { ChannelModel } from "@Services/mongodb/models/Channel"
 import { toURL } from "@Utils/helpers/toURL"
 import { badWordFilter } from "@Utils/helpers/badWordFilter"
 import { sanitize } from "@Services/mongodb/utils/sanitize"
@@ -9,28 +9,28 @@ import { sanitize } from "@Services/mongodb/utils/sanitize"
 const handler = createHandler()
 
 // Add a channel
-handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+handler.post(async (req: NextApiRequest, res: NextApiResponse<NextServerPayload<ChannelModel>>) => {
 	const { displayName, userId } = JSON.parse(req.body)
 
 	try {
 		if (displayName.length > 26)
-			return res.status(400).json({ error: true, message: "That name is too long. 25 characters or less" })
+			return res.status(400).json({ error: true, errorMessage: "That name is too long. 25 characters or less" })
 
 		if (badWordFilter(displayName)) {
-			return res.status(400).json({ error: true, message: "Hey, no bad words!" })
+			return res.status(400).json({ error: true, errorMessage: "Hey, no bad words!" })
 		}
 
 		const existingChannels = await Channel.find({ urlSafeName: toURL(displayName.trim()) })
 		if (existingChannels.length > 0) {
 			return res.status(403).json({
 				error: true,
-				message:
+				errorMessage:
 					"That name is too similar to another channel. We don't want to confuse our system...Please choose another."
 			})
 		} else {
 			const _id = new mongoose.Types.ObjectId()
 
-			const newUser = await new Channel({
+			const newUser = new Channel({
 				_id,
 				isPremium: false,
 				createdDate: Date.now(),
@@ -47,13 +47,13 @@ handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
 				]
 			})
 
-			const saveResult = await newUser.save({ new: true })
-			return res.status(200).json({ success: true, saveResult })
+			const data = await newUser.save()
+			return res.status(200).json(data)
 		}
 	} catch (error) {
 		return res.status(500).send({
 			error: true,
-			message: "We messed up. Error Code 4923"
+			errorMessage: "We messed up. Error Code 4923"
 		})
 	}
 })
