f862d11613551b0d80f7e2fe090d11cb161fee3d
Anthony Shew <anthonyshew@gmail.com>
2022-02-03 14:33:30 -0700

    Standardize usage of link editor.

--- web/src/pages/api/channel/meta/links.api.ts
+++ web/src/pages/api/channel/meta/links.api.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 import type { NextApiRequest, NextApiResponse } from "next"
 import { createHandler } from "@Middlewares/createHandler"
-import Channel from "@Services/mongodb/models/Channel"
+import Channel, { ChannelModel } from "@Services/mongodb/models/Channel"
 import { userAuth } from "@Utils/middlewares/auth"
 import { sanitize } from "@Services/mongodb/utils/sanitize"
 
 const handler = createHandler(userAuth)
 
 // Edit channel's links
-handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
+handler.put(async (req: NextApiRequest, res: NextApiResponse<NextServerPayload<ChannelModel | null>>) => {
 	const { _id, links } = req.body
 
 	try {
@@ -25,7 +25,7 @@ handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
 		)
 		return res.status(200).json(data)
 	} catch (error) {
-		return res.status(400).json(error)
+		return res.status(400).json({ error: true, errorMessage: JSON.stringify(error) })
 	}
 })
 
