66c24405f98b9805b374808f0e9dec12653e1e1d
Anthony Shew <anthonyshew@gmail.com>
2022-02-04 16:05:53 -0700

    Refactor: Saves with new Mongoose version.

--- web/src/pages/api/admin/games.api.ts
+++ web/src/pages/api/admin/games.api.ts
@@ -2,33 +2,37 @@ import mongoose from "mongoose"
 import type { NextApiRequest, NextApiResponse } from "next"
 import { createHandler } from "@Utils/middlewares/createHandler"
 import { adminAuth } from "@Utils/middlewares/auth"
-import { Game } from "@Services/mongodb/models"
+import Game, { GameModel } from "@Models/Game"
 
 const handler = createHandler(adminAuth)
 
 // Create a game
-handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
-	const newGame = await new Game(req.body)
+handler.post(async (req: NextApiRequest, res: NextApiResponse<NextServerPayload<GameModel>>) => {
+	const newGame = new Game(req.body)
 
-	await newGame.save((errors: any, data: Array<IGame>) => {
-		if (errors) return res.status(400).json(errors)
-		return res.status(200).json(data)
-	})
+	try {
+		const game = await newGame.save()
+		return res.status(200).json(game)
+	} catch (error) {
+		return res.status(400).json({ error: true, errorMessage: JSON.stringify(error) })
+	}
 })
 
 //Edit a game
-handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
+handler.put(async (req: NextApiRequest, res: NextApiResponse<NextServerPayload<GameModel | null>>) => {
 	const { _id, displayName, backgroundImage, titleImage } = req.body
 
-	Game.findOneAndUpdate(
-		{ _id: new mongoose.Types.ObjectId(_id) },
-		{ $set: { displayName, backgroundImage, titleImage } },
-		{ new: true },
-		(errors, data: IGame) => {
-			if (errors) return res.status(400).json(errors)
-			return res.status(200).json(data)
-		}
-	)
+	try {
+		const game = await Game.findOneAndUpdate(
+			{ _id: new mongoose.Types.ObjectId(_id) },
+			{ $set: { displayName, backgroundImage, titleImage } },
+			{ new: true }
+		)
+
+		return res.status(200).json(game)
+	} catch (error) {
+		return res.status(400).json({ error: true, errorMessage: JSON.stringify(error) })
+	}
 })
 
 export default handler
--- web/src/pages/api/admin/kits/base.api.ts
+++ web/src/pages/api/admin/kits/base.api.ts
@@ -1,28 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { createHandler } from "@Utils/middlewares/createHandler"
 import { adminAuth } from "@Utils/middlewares/auth"
-import { KitBase } from "@Services/mongodb/models"
+import KitBase, { KitBaseModel } from "@Services/mongodb/models/KitBase"
 
 const handler = createHandler(adminAuth)
 
 // Create a new kit base
-handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+handler.post(async (req: NextApiRequest, res: NextApiResponse<NextServerPayload<KitBaseModel>>) => {
 	const newKitBase = new KitBase(req.body)
 
-	await newKitBase.save({ new: true }, (errors: any, data: any) => {
-		if (errors) return res.status(400).json(errors)
-		return res.status(200).json(data)
-	})
+	try {
+		const base = await newKitBase.save()
+
+		return res.status(200).json(base)
+	} catch (error) {
+		return res.status(400).json({ error: true, errorMessage: JSON.stringify(error) })
+	}
 })
 
 // Edit an existing kit base
-handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
+handler.put(async (req: NextApiRequest, res: NextApiResponse<NextServerPayload<KitBaseModel | null>>) => {
 	const { _id, ...rest } = req.body
 
-	KitBase.findByIdAndUpdate(_id, { $set: rest }, { new: true }, (errors, data: IKitBase) => {
-		if (errors) return res.status(400).json(errors)
-		return res.status(200).json(data || {})
-	})
+	try {
+		const base = await KitBase.findByIdAndUpdate(_id, { $set: rest }, { new: true })
+		return res.status(200).json(base)
+	} catch (error) {
+		return res.status(400).json({ error: true, errorMessage: JSON.stringify(error) })
+	}
 })
 
 // Delete a kit base
