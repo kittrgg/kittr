df5e699eda0e0e0f167f647e5616348d7c76ed5f
Anthony Shew <anthonyshew@gmail.com>
2022-02-07 12:35:36 -0700

    Setup Sentry Nextjs config.

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -6,8 +6,11 @@ import * as Sentry from "@sentry/nextjs"
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
+const environment = process.env.ENABLE_SEEDING ? "testing" : "production"
+
 Sentry.init({
 	dsn: SENTRY_DSN,
+	environment,
 	// Adjust this value in production, or use tracesSampler for greater control
 	tracesSampleRate: 1.0
 	// ...
