56e9d9da3d1cc71eacd4604d4467f3a4cb8cfdc0
Anthony Shew <anthonyshew@gmail.com>
2022-02-14 12:55:20 -0700

    Try to auto-merge dependabot.

--- web/sentry.client.config.js
+++ web/sentry.client.config.js
@@ -9,15 +9,17 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 const environment = process.env.NEXT_PUBLIC_ENABLE_SEEDING === "true" ? "testing" : process.env.NODE_ENV
 
 Sentry.init({
-	allowUrls: ["https://*.kittr.gg", "http://localhost:3000"],
+	allowUrls: ["https://kittr.gg", "https://*.kittr.gg", "http://localhost:3000"],
 	ignoreErrors: [
 		"Failed to load Stripe.js",
-		"UnknownError: Connection to Indexed Database server lost. Refresh the page to try again"
+		"UnknownError: Connection to Indexed Database server lost. Refresh the page to try again",
+		"UnknownError: Error looking up record in object store by key range",
+		"Non-Error promise rejection captured with value: undefined"
 	],
 	dsn: SENTRY_DSN,
 	environment,
 	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 1.0
+	tracesSampleRate: 0.25
 	// ...
 	// Note: if you want to override the automatic release value, do not set a
 	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
