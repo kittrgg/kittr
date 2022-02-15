7a8ca7fd3b100e8b07540044b9c6f9a777e75760
Anthony Shew <anthonyshew@gmail.com>
2022-02-15 13:43:15 -0700

    Tighten Kit Base model.

--- seeds/mongo/models/KitBase.ts
+++ seeds/mongo/models/KitBase.ts
@@ -6,8 +6,25 @@ const schema = new Schema({
   displayName: String,
   gameId: mongoose.Types.ObjectId,
   image: String,
-  gameInfo: {},
-  commandCodes: [],
+  gameInfo: {
+    blurb: String,
+    stats: {
+      accuracy: Number,
+      range: Number,
+      mobility: Number,
+      damage: Number,
+      fireRate: Number,
+      control: Number
+    },
+    availableOptions: [
+      {
+        orderPlacement: String,
+        optionId: mongoose.Types.ObjectId,
+      }
+    ],
+    maxOptions: Number,
+  },
+  commandCodes: [String],
 });
 
 const Model = mongoose.models[MODEL_NAME] || mongoose.model(MODEL_NAME, schema);
