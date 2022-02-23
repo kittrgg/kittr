072fb264c192df36e987200eed71fd74e82c0fa4
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-23 19:48:38 +0000

    Changed the Prettier File to a Javascript File and Linted all .ts and .tsx files in accordant to these linting settings.

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 2,
+	semi: false,
+	useTabs: true,
+	singleQuote: false,
+	quoteProps: "consistent",
+	jsxSingleQuote: false,
+	trailingComma: "none",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	endOfLine: "crlf"
+}
+
+// npx prettier --config ./.prettierrc.js  --write "./**/*.ts"
+// npx prettier --config ./.prettierrc.js  --write "./**/*.tsx"
