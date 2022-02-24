caba1e3e1b975094a18aafa8a87b42a855757567
Anthony Shew <anthonyshew@gmail.com>
2022-02-24 12:43:02 -0700

    Woohoo, jest!

---
+++ apps/api/src/__tests__/example.test.ts
@@ -0,0 +1,5 @@
+describe("Example Test for API", () => {
+	it("Example Test", () => {
+		expect(1).toBe(1)
+	})
+})
---
+++ apps/web/src/__tests__/example.test.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+describe("Example Test for API", () => {
+	it("Example Test", () => {
+		expect(1).toBe(1)
+	})
+})
