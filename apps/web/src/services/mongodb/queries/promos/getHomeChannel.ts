509e11c7724879b01823fd70fe36a36d71301d60
Anthony Shew <anthonyshew@gmail.com>
2022-02-27 02:02:33 -0700

    Still trying to get symlinks right apparently...

--- apps/web/src/services/mongodb/queries/promos/getHomeChannel.ts
+++ apps/web/src/services/mongodb/queries/promos/getHomeChannel.ts
@@ -1,5 +1,5 @@
 import { Promo } from "@Services/mongodb/models/Promo"
-import { logger } from "@Services/sentry"
+import { nextLogger } from "@kittr/logger"
 
 export const getHomeChannelPromo = async () => {
 	const rightNow = new Date().getTime()
@@ -23,7 +23,7 @@ export const getHomeChannelPromo = async () => {
 	}
 
 	if (result.length > 1) {
-		logger.log({ message: "Multiple home channel promos found" })
+		nextLogger.logError("Multiple home channel promos found")
 	}
 
 	return result[0]
