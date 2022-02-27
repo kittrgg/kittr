e6e38c7a89ee6ae753cc298a092d9d5efa350203
Anthony Shew <anthonyshew@gmail.com>
2022-02-27 00:41:02 -0700

    Restore units in husky precommit.

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -2,14 +2,13 @@ const buildModuleNameMapper =
 	require("@kittr/config/jest/buildModuleNameMapper").buildModuleNameMapper
 
 const SRC_PATH = "./src"
-const TS_CONFIG_PATH = "./tsconfig.json"
+const TS_CONFIG_PATH = require("./tsconfig.json")
 
 module.exports = {
+	...require("@kittr/config/jest/common"),
 	testEnvironment: "jest-environment-jsdom",
-	testPathIgnorePatterns: ["<rootDir>/node_modules/"],
 	roots: [SRC_PATH],
-	clearMocks: true,
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	preset: "",
 	moduleNameMapper: buildModuleNameMapper(SRC_PATH, TS_CONFIG_PATH),
 	setupFilesAfterEnv: ["<rootDir>/setupTests.js"]
 }
