d3617f19c4927cfa52c6afd02cb3187e3d5f3373
Anthony Shew <anthonyshew@gmail.com>
2022-06-12 12:22:33 -0700

    One more games file.

--- apps/web/src/server/controllers/games/index.ts
+++ apps/web/src/server/controllers/games/index.ts
@@ -1,14 +1,23 @@
 import { createController } from "@Server/createController"
-import { getAllGames } from "@Server/services/games"
-import superjson from "superjson"
+import * as GamesService from "@Server/services/games"
+import { z } from "zod"
 
 const listGames = createController().query("", {
 	async resolve() {
-		const result = await getAllGames()
+		const result = await GamesService.getAllGames()
 		return result
 	}
 })
 
+const getGame = createController().query("", {
+	input: z.string(),
+	async resolve({ input: id }) {
+		const game = await GamesService.getGame(id)
+		return game
+	}
+})
+
 export const GamesController = {
-	listGames
+	listGames,
+	getGame
 }
