91f03621c0ad9c4fdddb1bbc78a30757509740ad
abdiweyrah <abdi_weyrah@Outlook.com>
2022-06-17 13:47:07 -0600

    Refactor  page to use Mantine

--- apps/web/src/pages/about.page.tsx
+++ apps/web/src/pages/about.page.tsx
@@ -1,20 +1,25 @@
+import Link from "next/link"
+import { Title, createStyles, Grid } from "@mantine/core"
+
+import { Routes } from "@Utils/lookups/routes"
+import { useViewportDimensions } from "@Hooks/useViewportDimensions"
 import colors from "@Colors"
 import PageWrapper from "@Components/layouts/PageWrapper"
-import { useViewportDimensions } from "@Hooks/useViewportDimensions"
-import { header1, header2 } from "@Styles/typography"
-import { Routes } from "@Utils/lookups/routes"
-import Link from "next/link"
-import styled from "styled-components"
 
 const About = ({ ...props }) => {
 	const { width } = useViewportDimensions()
+	const { classes } = useStyles()
 
 	return (
 		<PageWrapper title="About Us | kittr" description="Learn about the two of us that make kittr.">
-			<H1>ABOUT</H1>
-			<Grid>
-				<Container style={{ gridColumn: "span 3" }}>
-					<H2>OUR STORY</H2>
+			<Title order={1} className={classes.h1}>
+				ABOUT
+			</Title>
+			<Grid className={classes.grid}>
+				<Grid.Col className={classes.container} style={{ gridColumn: "span 3" }}>
+					<Title order={2} className={classes.h2}>
+						OUR STORY
+					</Title>
 					<p>
 						One day, our founder, Anthony, was watching some Warzone channels on Twitch. He used{" "}
 						<strong>an !amax command</strong> to try to find out about a player's CR-56 AMAX setup (classic 2020). In
@@ -32,35 +37,49 @@ const About = ({ ...props }) => {
 						Today, kittr is the <strong>kit sharing solution</strong> that video game players have been waiting for. We
 						hope you like it as much as we do.
 					</p>
-				</Container>
-				<Container style={{ gridColumn: (width as number) > 1000 ? "span 2" : "1 / span 1" }}>
-					<H2>WHAT WE DO</H2>
+				</Grid.Col>
+				<Grid.Col
+					className={classes.container}
+					style={{ gridColumn: (width as number) > 1000 ? "span 2" : "1 / span 1" }}
+				>
+					<Title order={2} className={classes.h2}>
+						WHAT WE DO
+					</Title>
 					<p>
 						kittr is the place for players to{" "}
 						<strong>easily share kits with their communities in a really nice looking way.</strong> We took it upon
 						ourselves to enhance the Twitch experience for viewers, mods, and players alike. By providing an easy-to-use
 						dashboard for player and moderators to create and edit kits, everyone wins!
 					</p>
-				</Container>
-				<Container style={{ gridColumn: (width as number) > 1000 ? "span 1" : "1 / span 1" }}>
-					<H2>WHO WE SERVE</H2>
+				</Grid.Col>
+				<Grid.Col
+					className={classes.container}
+					style={{ gridColumn: (width as number) > 1000 ? "span 1" : "1 / span 1" }}
+				>
+					<Title order={2} className={classes.h2}>
+						WHO WE SERVE
+					</Title>
 					<p>
 						Twitch <strong>players</strong>, <strong>moderators</strong>, and <strong>viewers</strong> all love using
 						kittr because it looks great and is easy to use. But don't take our word for it. Check out the channels of
 						the channels on <Link href={Routes.CHANNEL.LIST}>our channels</Link> and watch kittr in action. We think
 						you'll be convinced.
 					</p>
-				</Container>
-				<Container style={{ gridColumn: "span 3" }}>
-					<H2>OUR MISSION</H2>
+				</Grid.Col>
+				<Grid.Col className={classes.container} style={{ gridColumn: "span 3" }}>
+					<Title order={2} className={classes.h2}>
+						OUR MISSION
+					</Title>
 					<p>
 						We strive to create a <strong>better channeling experience</strong> for Twitch player, moderators, and
 						viewers alike. Before kittr, there wasn’t a great way for player to share their kits with viewers. It’s 2021
 						- Google Sheets just won’t cut it. But don’t worry, we here at kittr have got your back.
 					</p>
-				</Container>
-				<Container style={{ gridColumn: "span 3" }}>
-					<H2>CONTACT US</H2>
+				</Grid.Col>
+				<Grid.Col className={classes.container} style={{ gridColumn: "span 3" }}>
+					<Title order={2} className={classes.h2}>
+						CONTACT US
+					</Title>
 					<p>
 						If you have any questions, try visiting{" "}
 						<Link href={Routes.FAQ}>
@@ -69,7 +88,7 @@ const About = ({ ...props }) => {
 						. If you don't find the answer you are looking for there, head over or{" "}
 						<Link href={Routes.CONTACT}>our contact page</Link>.
 					</p>
-				</Container>
+				</Grid.Col>
 			</Grid>
 		</PageWrapper>
 	)
@@ -77,56 +96,66 @@ const About = ({ ...props }) => {
 
 export default About
 
-// Styled Components
+const useStyles = createStyles((theme) => {
+	return {
+		// heading styles will be made global
+		h1: {
+			marginBottom: "24px",
+			padding: "0 5%",
+			fontSize: "36px",
+			fontWeight: 900,
+			letterSpacing: "3px",
+			fontFamily: '"Barlow Condensed", serif',
+			color: theme.white
+		},
 
-const H1 = styled.h1`
-	margin-bottom: 24px;
-	padding: 0 5%;
-	${header1};
-`
+		h2: {
+			marginBottom: "12px",
+			color: theme.white,
+			fontSize: "22px",
+			fontWeight: 600,
+			letterSpacing: "2px",
+			fontFamily: '"Montserrat", sans-serif'
+		},
 
-const Grid = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr 1fr;
-	gap: 24px;
-	padding: 0 5%;
+		grid: {
+			display: "grid",
+			gridTemplateColumns: "1fr 1fr 1fr",
+			gap: "24px",
+			padding: "0 5%",
 
-	@media (max-width: 1000px) {
-		grid-template-columns: 1fr;
-		column-gap: 0;
-	}
-`
+			["@media (max-width: 1000px)"]: {
+				gridTemplateColumns: "1fr",
+				columnGap: "0"
+			}
+		},
 
-const Container = styled.div`
-	padding: 32px;
-	border-radius: 10px;
-	background-color: ${colors.darker};
-	color: ${colors.lighter};
-	line-height: 200%;
+		container: {
+			padding: "32px",
+			borderRadius: "10px",
+			backgroundColor: colors.darker,
+			color: colors.lighter,
+			lineHeight: "200%",
 
-	p {
-		font-size: 18px;
+			["& p"]: {
+				fontSize: "18px",
 
-		&:not(:nth-child(1)) {
-			margin-top: 24px;
+				["&:not(:nth-child(1))"]: {
+					marginTop: "24px"
+				},
+
+				["@media (max-width: 600px)"]: {
+					fontSize: "14px"
+				}
+			},
+
+			["& strong"]: {
+				fontWeight: 600
+			},
+
+			["& a"]: {
+				color: colors.lighter
+			}
 		}
-
-		@media (max-width: 600px) {
-			font-size: 14px;
-		}
 	}
-
-	strong {
-		font-weight: 600;
-	}
-
-	a {
-		color: ${colors.lighter};
-	}
-`
-
-const H2 = styled.h2`
-	margin-bottom: 12px;
-	color: ${colors.white};
-	${header2};
-`
+})
