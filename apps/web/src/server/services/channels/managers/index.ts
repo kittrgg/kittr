741bbf2a939a5e79961cf195e9ffc81f1c2c0f7f
Anthony Shew <anthonyshew@gmail.com>
2022-06-17 09:20:34 -0700

    Fix: Role based access for promote/demote.

--- apps/web/src/server/services/channels/managers/index.ts
+++ apps/web/src/server/services/channels/managers/index.ts
@@ -1,6 +1,22 @@
 import { prisma } from "@kittr/prisma"
+import { TRPCError } from "@trpc/server"
 
 export const promoteManager = async ({ channelId, managerId }: { channelId: string; managerId: string }) => {
+	const manager = await prisma.channelManager.findFirst({
+		where: {
+			channelId,
+			id: managerId,
+			role: "OWNER" || "ADMIN"
+		}
+	})
+
+	if (!manager) {
+		throw new TRPCError({
+			code: "UNAUTHORIZED",
+			message: "You do not have permission to do promote managers on this channel."
+		})
+	}
+
 	const channel = await prisma.channel.update({
 		where: {
 			id: channelId
@@ -23,6 +39,21 @@ export const promoteManager = async ({ channelId, managerId }: { channelId: stri
 }
 
 export const demoteManager = async ({ channelId, managerId }: { channelId: string; managerId: string }) => {
+	const manager = await prisma.channelManager.findFirst({
+		where: {
+			channelId,
+			id: managerId,
+			role: "OWNER" || "ADMIN"
+		}
+	})
+
+	if (!manager) {
+		throw new TRPCError({
+			code: "UNAUTHORIZED",
+			message: "You do not have permission to do promote managers on this channel."
+		})
+	}
+
 	const channel = await prisma.channel.update({
 		where: {
 			id: channelId
