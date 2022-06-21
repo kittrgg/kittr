66bca9b35efdce70a874744dd29f5bcde589498c
Anthony Shew <anthonyshew@gmail.com>
2022-06-21 10:59:02 -0700

    Fix: updating links

--- apps/web/src/server/services/channels/links/index.ts
+++ apps/web/src/server/services/channels/links/index.ts
@@ -1,11 +1,24 @@
 import { prisma, ChannelLink } from "@kittr/prisma"
 
 export const updateLinks = async ({ channelId, links }: { channelId: string; links: ChannelLink[] }) => {
-	const linksUpdate = await prisma.channelLink.updateMany({
+	const linksUpdate = await prisma.channel.update({
 		where: {
-			channelId
+			id: channelId
 		},
-		data: links
+		data: {
+			links: {
+				connectOrCreate: links.map((link) => {
+					const { channelId, ...rest } = link
+
+					return {
+						where: {
+							id: link.id
+						},
+						create: rest
+					}
+				})
+			}
+		}
 	})
 
 	return linksUpdate
