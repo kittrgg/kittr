89fd4fd0587996ac81202edc5d7eca9b78d0f771
Jacob S <jacobraysickels@gmail.com>
2022-06-22 19:47:06 -0500

    Lets make an delete kit on kittr lmao

--- apps/web/src/server/controllers/channels/kits/index.ts
+++ apps/web/src/server/controllers/channels/kits/index.ts
@@ -1,14 +1,14 @@
+import { KitModel, KitOptionModel } from "@kittr/prisma/validator"
 import { createController } from "@Server/createController"
 import * as ChannelsService from "@Server/services/channels"
 import { z } from "zod"
-import { KitOptionModel, KitModel } from "@kittr/prisma/validator"
 
 const upsertKitToChannel = createController().mutation("", {
 	input: z.object({
 		channelId: z.string(),
 		kit: KitModel.extend({
 			options: KitOptionModel.array().default([])
-		})
+		}).partial({ id: true })
 	}),
 	async resolve({ input }) {
 		const channel = await ChannelsService.upsertKit({ channelId: input.channelId, kit: input.kit })
