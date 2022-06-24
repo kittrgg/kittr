782d69e175774db3cf3490c68d76fc84d9a19d42
opti21 <40129778+opti21@users.noreply.github.com>
2022-06-24 15:45:58 -0500

    chore: cleaned up unneeded code

--- apps/web/src/pages/why-kittr/Hero.tsx
+++ apps/web/src/pages/why-kittr/Hero.tsx
@@ -1,8 +1,6 @@
 import colors from "@Colors"
-import styled from "styled-components"
-import { header1, header2 } from "@Styles/typography"
-import { Text, Title } from "@kittr/ui"
-import { BackgroundImage, Box, Center, Image, Stack } from "@mantine/core"
+import { Title } from "@kittr/ui"
+import { Box, Image, Stack } from "@mantine/core"
 
 interface Props {
 	totalNumberOfKits: number
@@ -42,76 +40,7 @@ const Hero = ({ totalNumberOfKits }: Props) => {
 				Quite a bit better than a Google Sheet, huh?
 			</Title>
 		</Stack>
-		// <Box // 	sx={() => ({
-		// 		position: "relative",
-		// 		zIndex: -1,
-		// 		top: "-150px",
-		// 		marginBottom: "-50vh",
-		// 		height: "100vh"
-		// 	})}
-		// >
-		// 	<Box
-		// 		sx={() => ({
-		// 			position: "absolute",
-		// 			inset: 0,
-		// 			width: "100%",
-		// 			height: "100vh",
-		// 			overflow: "hidden",
-		// 			filter: "blur(4px)"
-		// 		})}
-		// 	>
-		// 		<Image src="/media/hero.jpg" width={"100%"} height={"100%"} fit="cover" />
-		// 	</Box>
-		// 	<Box
-		// 		sx={() => ({
-		// 			position: "absolute",
-		// 			top: "22%",
-		// 			left: "50%",
-		// 			transform: "translateX(-50%)",
-		// 			width: "100%",
-		// 			zIndex: 1,
-		// 			display: "flex",
-		// 			flexDirection: "column",
-		// 			alignItems: "center",
-		// 			justifyContent: "center"
-		// 		})}
-		// 	>
-		// 		<Image src="/media/logo.svg" alt="Kittr Logo" width={"100%"} height={"100%"} />
-		// 		<Title preset="d1" m="lg">
-		// 			get kittd.
-		// 		</Title>
-		// 		<Title preset="h2" style={{ color: colors.lightest }}>
-		// 			{totalNumberOfKits}+ kits
-		// 		</Title>
-		// 		<Title preset="h3" mt={10}>
-		// 			In 15 minutes, your !loadout command can look like this.
-		// 		</Title>
-		// 	</Box>
-		// </Box>
 	)
 }
 
 export default Hero
-
-// Styled Components
-
-const Copy = styled.div``
-
-const Headline = styled.h2`
-	${header1};
-	font-size: 72px;
-`
-
-const Subline = styled.h3`
-	margin-top: 18px;
-	padding: 0 10%;
-	font-weight: 600;
-	font-size: 22px;
-	letter-spacing: 2px;
-	text-align: center;
-`
-
-const KitsTotal = styled.p`
-	color: ${colors.lightest};
-	${header2};
-`
