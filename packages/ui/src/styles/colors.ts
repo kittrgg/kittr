6ac9002589f77c249a2e41aa62388ab87c2fc8dc
opti21 <40129778+opti21@users.noreply.github.com>
2022-06-24 18:16:17 -0500

    feat: added colors to ui

---
+++ packages/ui/src/styles/colors.ts
@@ -0,0 +1,50 @@
+const colors = {
+	/** #ffffff */
+	white: "#ffffff",
+	/** #636363 */
+	lightest: "#636363",
+	/** #9B9B9B */
+	lighter: "#9B9B9B",
+	/** #3D3C40 */
+	light: "#3D3C40",
+	/** #1D1D1F */
+	middle: "#1D1D1F",
+	/** #801D1D1F */
+	middleOpacity98: "#1D1D1FFA",
+	/** #801D1DE6 */
+	middleOpacity90: "#1D1D1FE6",
+	/** #262628 */
+	dark20: "#262628",
+	/** #2F2F31 */
+	darker: "#2F2F31",
+	/** #121212 */
+	dark: "#121212",
+	/** #000000 */
+	black: "#000000",
+	/** #C04040 */
+	red: "#C04040",
+	/** #603333 */
+	darkRed: "#603333",
+	/** #AA8F00 */
+	gold: "#AA8F00",
+	/** #096613 */
+	green: "#096613",
+	/** #8C45F7 */
+	twitchPurple: "#8C45F7",
+	/** #1D89CE */
+	nightbotBlue: "#1D89CE",
+	/** #5684fd */
+	channelElementsBlue: "#5684fd",
+	/** #8C45F7 */
+	premium: "#8C45F7",
+	/** #D5B9FF */
+	premiumLight: "#D5B9FF",
+	/** #6F31CD */
+	premiumDark: "#6F31CD",
+	/** #6F31CD80 */
+	premiumDarkOpacity50: "#6F31CD80",
+	/** background: linear-gradient(90deg, #8C45F7 0%, #B345F7 100%) */
+	premiumGradient: "linear-gradient(90deg, #8C45F7 0%, #B345F7 100%)"
+}
+
+export default colors
