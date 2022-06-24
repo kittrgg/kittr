42794dab80f506f66177257adcb4760a95d9e1a9
opti21 <40129778+opti21@users.noreply.github.com>
2022-06-24 18:15:26 -0500

    feat: Created card component

---
+++ packages/ui/src/components/Card/index.tsx
@@ -0,0 +1,58 @@
+import { Box, BoxProps, Image } from "@mantine/core"
+import colors from "../../styles/colors"
+import { Title } from "../Title"
+
+const CARD_PRESETS = {
+	lg: {
+		width: "100%",
+		backgroundColor: colors.darker,
+		padding: "24px",
+		borderRadius: "12px",
+		minHeigh: "300px"
+	}
+}
+
+interface Props extends BoxProps<any> {
+	/** Use a preset defined styling property. */
+	preset?: keyof typeof CARD_PRESETS
+	title: string
+	icon?: string
+	iconAlt?: string
+}
+
+export const Card = ({
+	preset,
+	title,
+	icon,
+	iconAlt,
+	children,
+	sx,
+	...props
+}: Props) => {
+	const styles = preset ? CARD_PRESETS[preset] : CARD_PRESETS["lg"]
+	// const { colors } = useMantineTheme()
+
+	return (
+		<Box sx={{ ...styles, ...sx, textAlign: "center" }} {...props}>
+			<Box
+				sx={() => ({
+					width: "100%"
+				})}
+			>
+				{icon && (
+					<Image
+						src={icon}
+						width="100%"
+						height="100px"
+						fit="contain"
+						alt={iconAlt}
+					/>
+				)}
+			</Box>
+			<Title preset="h3" mt="md">
+				{title}
+			</Title>
+			{children}
+		</Box>
+	)
+}
