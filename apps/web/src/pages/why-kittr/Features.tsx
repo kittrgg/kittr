0f348556620236d9685c0a4425ad2aa47c84cccf
opti21 <40129778+opti21@users.noreply.github.com>
2022-06-24 20:04:32 -0500

    fix: better mantine use on feature comp

--- apps/web/src/pages/why-kittr/Features.tsx
+++ apps/web/src/pages/why-kittr/Features.tsx
@@ -1,16 +1,18 @@
 import { Card, Title, Text } from "@kittr/ui"
-import { Grid, Stack } from "@mantine/core"
+import { Center, Grid, Stack } from "@mantine/core"
 import { Routes } from "@Utils/lookups/routes"
 import CallToAction from "./CallToAction"
 
 /** Features that are currently a part of the platform */
 const Features = () => {
 	return (
-		<Stack align="center" mt={40}>
-			<Title preset="h1">FEATURES</Title>
+		<>
+			<Center pb={20} pt={40}>
+				<Title preset="h1">FEATURES</Title>
+			</Center>
 
-			<Grid gutter={40}>
-				<Grid.Col key={1} md={6} lg={3}>
+			<Grid gutter={30} pb={20}>
+				<Grid.Col sm={6} lg={3}>
 					<Card title={"Kit Displayr"} icon={"/media/icons/displayr.svg"} iconAlt="Displayr Icon">
 						<Text preset="lg" mt="md">
 							Stop trying to make Google Sheets look good. We've done the work of making your loadouts beautiful by
@@ -26,7 +28,7 @@ const Features = () => {
 						</Text>
 					</Card>
 				</Grid.Col>
-				<Grid.Col key={1} md={6} lg={3}>
+				<Grid.Col sm={6} lg={3}>
 					<Card title="Kit Editr" icon="/media/icons/editr.svg" iconAlt="Editr Icon">
 						<Text preset="lg" mt="md">
 							Collaboratively edit your kits faster, more accurately, and more easily. It takes about 10 clicks to fully
@@ -34,7 +36,7 @@ const Features = () => {
 						</Text>
 					</Card>
 				</Grid.Col>
-				<Grid.Col key={1} md={6} lg={3}>
+				<Grid.Col sm={6} lg={3}>
 					<Card title="Youtube Emebeds" icon="/media/icons/youtube-embed.svg" iconAlt="Youtube Embed Icon">
 						<Text preset="lg" mt="md">
 							Let's mix in your YouTube content in a way that people will <em>want</em> to watch it. On our interface,
@@ -42,7 +44,7 @@ const Features = () => {
 						</Text>
 					</Card>
 				</Grid.Col>
-				<Grid.Col key={1} md={6} lg={3}>
+				<Grid.Col sm={6} lg={3}>
 					<Card title="Bot Command Expotr" icon="/media/icons/exportr.svg" iconAlt="Exportr Icon">
 						<Text preset="lg" mt="md">
 							Keep your bot updated with one click and a couple keystrokes. We create your updates for you so that you
@@ -52,7 +54,7 @@ const Features = () => {
 				</Grid.Col>
 			</Grid>
 			<CallToAction marginBottom="60px" header="channel bettr with kittr." />
-		</Stack>
+		</>
 	)
 }
 
