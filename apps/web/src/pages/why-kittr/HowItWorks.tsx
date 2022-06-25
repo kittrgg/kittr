407d25411bd1ae25a136a66a05500baa57c9a666
opti21 <40129778+opti21@users.noreply.github.com>
2022-06-24 19:37:19 -0500

    chore: HowItWorks to mantine

--- apps/web/src/pages/why-kittr/HowItWorks.tsx
+++ apps/web/src/pages/why-kittr/HowItWorks.tsx
@@ -1,4 +1,6 @@
 import colors from "@Colors"
+import { Section, Text, Title } from "@kittr/ui"
+import { Box, Grid, Image } from "@mantine/core"
 import { header1, header2 } from "@Styles/typography"
 import styled from "styled-components"
 import CallToAction from "./CallToAction"
@@ -6,24 +8,27 @@ import * as Styled from "./style"
 
 const STEPS = [
 	{
+		id: 1,
 		title: "STEP 1: CREATE YOUR KITS",
-		titleHelper: "5-13 minutes",
+		titleHelper: " (5-13 minutes)",
 		image: "/media/kit-editor-example.png",
 		imageWidth: 1679,
 		imageHeight: 977,
 		border: true
 	},
 	{
+		id: 2,
 		title: "STEP 2: UPDATE YOUR COMMANDS",
-		titleHelper: "2-4 minutes",
+		titleHelper: " (2-4 minutes)",
 		image: "/media/export-commands-example.jpg",
 		imageWidth: 1440,
 		imageHeight: 1022,
 		border: true
 	},
 	{
+		id: 3,
 		title: "STEP 3: TURN THE CHANNEL ON",
-		titleHelper: "!loadouts",
+		titleHelper: " (!loadouts)",
 		image: "/media/kit-stack.png",
 		imageWidth: 1440,
 		imageHeight: 1022,
@@ -33,67 +38,35 @@ const STEPS = [
 
 const HowItWorks = ({}) => {
 	return (
-		<section style={{ maxWidth: "1000px", margin: "0 auto" }}>
-			<Styled.H2>HOW IT WORKS</Styled.H2>
-			{STEPS.map((step) => {
-				return (
-					<StepContainer key={step.title}>
-						<StepHeaderFlex>
-							<StepHeader>{step.title}</StepHeader>
-							<StepHeaderHelper>{step.titleHelper}</StepHeaderHelper>
-						</StepHeaderFlex>
-						<StepImageContainer border={step.border}>
-							<img src={step.image} alt="" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-						</StepImageContainer>
-					</StepContainer>
-				)
-			})}
+		<Box style={{ maxWidth: "1000px", margin: "0 auto", textAlign: "center" }}>
+			<Title preset="h1" mb={20}>
+				HOW IT WORKS
+			</Title>
+			<Grid gutter="xl">
+				{STEPS.map((step) => {
+					return (
+						<Grid.Col>
+							<Section key={step.id} title={step.title + step.titleHelper}>
+								<Image
+									src={step.image}
+									width={"100%"}
+									height={"100%"}
+									radius="md"
+									p={10}
+									sx={() => ({
+										backgroundColor: colors.darker,
+										borderRadius: "12px"
+									})}
+								/>
+							</Section>
+						</Grid.Col>
+					)
+				})}
+			</Grid>
+
 			<CallToAction marginTop="60px" marginBottom="48px" header="start sharing today." />
-		</section>
+		</Box>
 	)
 }
 
 export default HowItWorks
-
-// Styled Components
-
-const StepContainer = styled.div`
-	margin-top: 64px;
-`
-
-const StepHeaderFlex = styled.div`
-	position: relative;
-	width: 100%;
-	display: flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	align-items: center;
-	justify-content: space-between;
-`
-
-const StepHeader = styled.h4`
-	${header1};
-	font-weight: initial;
-
-	@media (max-width: 450px) {
-		margin-left: 0;
-		font-size: 32px;
-	}
-`
-
-const StepHeaderHelper = styled.p`
-	margin-left: 32px;
-	color: ${colors.lighter};
-	${header2};
-	font-size: 20px;
-`
-
-const StepImageContainer = styled.div<{ border: boolean }>`
-	position: relative;
-	width: 50vw;
-	min-width: 320px;
-	margin: 36px auto;
-	overflow: hidden;
-	box-shadow: ${(props) => (props.border ? "0 0 0 10px rgba(255,255,255, .05)" : "")};
-	border-radius: 12px;
-`
