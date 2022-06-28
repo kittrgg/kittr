0ae61766d6dcad690a5ae0c71fc41c76afbc4a67
Anthony Shew <anthonyshew@gmail.com>
2022-06-27 21:50:04 -0700

    Remove search.

--- apps/web/src/pages/channels/search/[searchTerm].page.tsx
+++ apps/web/src/pages/channels/search/[searchTerm].page.tsx
@@ -7,46 +7,49 @@ import AdPageWrapper, { H1 } from "@Components/layouts/AdPageWrapper"
 import { ChannelSearch, ChannelList, Spinner } from "@Components/shared"
 import ResponsiveBanner from "@Services/venatus/ResponsiveBanner"
 
-const ChannelsSearch = ({ ...props }) => {
+const ChannelsSearch = () => {
 	const { width } = useViewportDimensions()
 	const {
 		query: { searchTerm }
 	} = useRouter()
-	const { data, isFetching } = useChannelSearch(searchTerm as string)
+	// const { data, isFetching } = useChannelSearch(searchTerm as string)
 
-	return (
-		<AdPageWrapper
-			title={`Search Channels: ${searchTerm || "?"}`}
-			description={`Look for ${searchTerm || "?"} on kittr.`}
-		>
-			<ChannelSearch />
-			<H1>CHANNELS</H1>
-			{isFetching && <Spinner />}
-			{!isFetching && data && (
-				<>
-					<div style={{ margin: "0 5%" }}>
-						<ChannelList data={data.slice(0, 20)} />
-					</div>
-					{data.length > 20 && (
-						<ParagraphWrapper>
-							<Paragraph>
-								More than 20 channels matched your search. { "Didn't" }find who you were looking for? Refine your search term
-								a little.
-							</Paragraph>
-						</ParagraphWrapper>
-					)}
-					{data.length === 0 && (
-						<ParagraphWrapper>
-							<Paragraph>
-								Hmm, no matches there. You have to get within two characters to find who you are looking for.
-							</Paragraph>
-						</ParagraphWrapper>
-					)}
-				</>
-			)}
-			{width < 1200 && <ResponsiveBanner largeWidthAdUnit="d728x90" smallWidthAdUnit="s300x250" />}
-		</AdPageWrapper>
-	)
+	// TODO: Re-enable search functionality!
+return <p>404</p>
+
+	// return (
+	// 	<AdPageWrapper
+	// 		title={`Search Channels: ${searchTerm || "?"}`}
+	// 		description={`Look for ${searchTerm || "?"} on kittr.`}
+	// 	>
+	// 		<ChannelSearch />
+	// 		<H1>CHANNELS</H1>
+	// 		{isFetching && <Spinner />}
+	// 		{!isFetching && data && (
+	// 			<>
+	// 				<div style={{ margin: "0 5%" }}>
+	// 					<ChannelList data={data.slice(0, 20)} />
+	// 				</div>
+	// 				{data.length > 20 && (
+	// 					<ParagraphWrapper>
+	// 						<Paragraph>
+	// 							More than 20 channels matched your search. { "Didn't" }find who you were looking for? Refine your search term
+	// 							a little.
+	// 						</Paragraph>
+	// 					</ParagraphWrapper>
+	// 				)}
+	// 				{data.length === 0 && (
+	// 					<ParagraphWrapper>
+	// 						<Paragraph>
+	// 							Hmm, no matches there. You have to get within two characters to find who you are looking for.
+	// 						</Paragraph>
+	// 					</ParagraphWrapper>
+	// 				)}
+	// 			</>
+	// 		)}
+	// 		{width < 1200 && <ResponsiveBanner largeWidthAdUnit="d728x90" smallWidthAdUnit="s300x250" />}
+	// 	</AdPageWrapper>
+	// )
 }
 
 export default ChannelsSearch
