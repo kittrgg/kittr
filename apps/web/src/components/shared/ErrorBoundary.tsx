d01767b916a9fdc89013b069620adf87ea7c4ae0
Anthony Shew <anthonyshew@gmail.com>
2022-07-03 09:19:47 -0700

    Testing if these are ad errors in logs.

--- apps/web/src/components/shared/ErrorBoundary.tsx
+++ apps/web/src/components/shared/ErrorBoundary.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react"
+import { logWarning } from "@kittr/logger/nextjs"
 
 export class ErrorBoundary extends React.Component {
-  constructor(props: any) {
-    super(props)
-    this.state = { hasError: false }
-  }
+	constructor(props: any) {
+		super(props)
+		this.state = { hasError: false }
+	}
 
-  static getDerivedStateFromError(error: any) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true }
-  }
+	static getDerivedStateFromError(error: any) {
+		// Update state so the next render will show the fallback UI.
+		return { hasError: true }
+	}
 
-  componentDidCatch(error: any, errorInfo: any) {
-    // You can also log the error to an error reporting service
-  }
+	componentDidCatch(error: any, errorInfo: any) {
+		// You can also log the error to an error reporting service
+		logWarning("An ad error occurred.")
+	}
 
-  render() {
-    return this.props.children
-  }
+	render() {
+		return this.props.children
+	}
 }
