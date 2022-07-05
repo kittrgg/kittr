93d58616e7410779df323d15f33cdebfc91f4e66
Anthony Shew <anthonyshew@gmail.com>
2022-07-05 15:09:50 -0700

    Try to return 200s for OPTIONS method before req makes it to tRPC.

--- apps/web/src/pages/api/trpc/[[...trpc]].api.ts
+++ apps/web/src/pages/api/trpc/[[...trpc]].api.ts
@@ -1,15 +1,24 @@
 import * as trpcNext from "@trpc/server/adapters/next"
+import Cors from 'cors'
 import { createContext } from "@Server/context"
 import { appRouter } from "@Server/index"
+import { NextApiRequest, NextApiResponse, NextApiHandler } from "next"
 
 // export type definition of API
 export type AppRouter = typeof appRouter
 
+const handleOptionsMethod = (handler: NextApiHandler) => {
+	return async (req: NextApiRequest, res: NextApiResponse) => {
+		if (req.method === "OPTIONS") return res.send(200)
+		return handler(req, res)
+	}
+}
+
 // export API handler
-export default trpcNext.createNextApiHandler({
+export default handleOptionsMethod(trpcNext.createNextApiHandler({
 	router: appRouter,
 	createContext: createContext,
 	batching: {
 		enabled: true
 	}
-})
+}))
