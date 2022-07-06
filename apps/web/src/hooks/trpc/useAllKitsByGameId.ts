8e60181b88ff493b3425ecc800a2fbea6251906f
Anthony Shew <anthonyshew@gmail.com>
2022-07-05 18:47:27 -0700

    More logs, please.

--- apps/web/src/hooks/trpc/useAllKitsByGameId.ts
+++ apps/web/src/hooks/trpc/useAllKitsByGameId.ts
@@ -1,6 +1,9 @@
+import { captureException } from "@sentry/nextjs"
 import { trpc } from "@Server/createHooks"
 
 export const useAllKitsByGameId = (gameId: string) => {
-	const query = trpc.useQuery(["kits/bases/game-list", { game: gameId }])
+	const query = trpc.useQuery(["kits/bases/game-list", { game: gameId }], {
+		onError: (error) => captureException(error)
+	})
 	return query
 }
