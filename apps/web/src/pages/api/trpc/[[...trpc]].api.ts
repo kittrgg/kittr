54ce4fdf6916196dd125cae1103f65576ac9eba4
Anthony Shew <anthonyshew@gmail.com>
2022-07-05 21:25:26 -0700

    More logs.

--- apps/web/src/pages/api/trpc/[[...trpc]].api.ts
+++ apps/web/src/pages/api/trpc/[[...trpc]].api.ts
@@ -2,13 +2,16 @@ import * as trpcNext from "@trpc/server/adapters/next"
 import { createContext } from "@Server/context"
 import { appRouter } from "@Server/index"
 import { NextApiRequest, NextApiResponse, NextApiHandler } from "next"
+import { captureMessage } from "@sentry/nextjs"
 
 // export type definition of API
 export type AppRouter = typeof appRouter
 
 const handleOptionsMethod = (handler: NextApiHandler) => {
 	return async (req: NextApiRequest, res: NextApiResponse) => {
+		captureMessage(`User with auth: ${JSON.stringify(req.headers.authorization)}`)
 		if (req.method === "OPTIONS") return res.send(200)
+		captureMessage(`User with auth2: ${JSON.stringify(req.headers.authorization)}`)
 		return handler(req, res)
 	}
 }
