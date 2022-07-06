2bdacf187af38be4bd601da34a90147d62fbe1f4
Anthony Shew <anthonyshew@gmail.com>
2022-07-05 23:31:52 -0700

    Remvoe sandbox.

--- apps/web/src/pages/developers/sandbox.page.tsx
+++ apps/web/src/pages/developers/sandbox.page.tsx
@@ -5,14 +5,11 @@ export default function Sandbox() {
 	// 	logInfo("Test from client")
 	// }, [])
 
-	const query = trpc.useQuery(["error"])
-
 	return (
 		<div>
 			I am a sandbox for the developers.
 			{/* <Title>Do the thing</Title> */}
 			{/* <pre>{JSON.stringify(hello.data, null, 2)}</pre> */}
-			<p>{JSON.stringify(query.data)}</p>
 		</div>
 	)
 }
