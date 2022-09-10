b10de47dc33c30e534b5f90e33ee7dafcd94a890
Anthony Shew <anthony@inthezone.dev>
2022-09-09 18:28:54 -0700

    On vercel pretty well now.

--- apps/web/src/pages/_app.page.tsx
+++ apps/web/src/pages/_app.page.tsx
@@ -18,10 +18,10 @@ import { AppRouter } from "./api/trpc/[[...trpc]].api"
 import { getToken } from "@Services/firebase/auth"
 
 const AppWrap = ({ Component, pageProps }: any) => (
-		<Provider store={store}>
-			<MyApp Component={Component} pageProps={pageProps} />
-		</Provider>
-	)
+	<Provider store={store}>
+		<MyApp Component={Component} pageProps={pageProps} />
+	</Provider>
+)
 
 const MyApp = ({ Component, pageProps }: any) => {
 	const router = useRouter()
@@ -53,7 +53,7 @@ const MyApp = ({ Component, pageProps }: any) => {
 						marginBottom: "0 !important"
 					},
 					// Make sure that Venatus ads are always a minimum of 320px wide.
-					"[data-ref=\"vm-preloader\"]": {
+					'[data-ref="vm-preloader"]': {
 						minWidth: "320px !important"
 					},
 					// // This hides a warning about how you are using firebase emulators
@@ -76,22 +76,23 @@ const MyApp = ({ Component, pageProps }: any) => {
 }
 
 export default withTRPC<AppRouter>({
-	config ({}) {
+	config({}) {
 		/*
 		 * If you want to use SSR, you need to use the server's full URL
 		 * @link https://trpc.io/docs/ssr
 		 */
-		const url = (process.env.NEXT_PUBLIC_SOCKET_HOST as string)?.includes("stage")
-			? "https://stage-web.kittr.gg/api/trpc"
-			: (process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string)?.includes("prod")
-			? "https://kittr.gg/api/trpc"
-			: "http://localhost:3000/api/trpc"
+		const url =
+			process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+				? (process.env.NEXT_PUBLIC_VERCEL_URL as string)
+				: process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+				? "https://kittr.vercel.app/api/trpc"
+				: "http://localhost:3000/api/trpc"
 
 		return {
 			url,
 			headers: async () => ({
-					authorization: await getToken()
-				}),
+				authorization: await getToken()
+			}),
 			// fetch: async (requestUrl, test) => {
 			// 	return fetch(requestUrl, {
 			// 		...test,
@@ -102,7 +103,8 @@ export default withTRPC<AppRouter>({
 			links: [
 				// adds pretty logs to your console in development and logs errors in production
 				loggerLink({
-					enabled: (opts) => process.env.NODE_ENV === "development" || (opts.direction === "down" && opts.result instanceof Error)
+					enabled: (opts) =>
+						process.env.NODE_ENV === "development" || (opts.direction === "down" && opts.result instanceof Error)
 				}),
 				httpBatchLink({
 					url
