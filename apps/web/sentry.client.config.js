625ae208e38ee3c5b51a42ef38e2ded88b16fcd5
Anthony Shew <anthony@inthezone.dev>
2022-09-10 10:59:02 -0700

    Make sure error gets sent out.

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -11,6 +11,7 @@ Sentry.init({
 	tracesSampleRate: 1.0,
 	beforeSend: (event) => {
 		console.log(event)
+		return event
 	}
 	// Note: if you want to override the automatic release value, do not set a
 	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
