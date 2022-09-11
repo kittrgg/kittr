083ff23e342ee98d46ec43e4ea83a42f6fd6af4a
Anthony Shew <anthony@inthezone.dev>
2022-09-11 10:05:44 -0700

    Fix url.

--- apps/web/src/utils/helpers/getUrl.ts
+++ apps/web/src/utils/helpers/getUrl.ts
@@ -2,11 +2,11 @@ export const getTrpcUrl =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
     ? "/api/trpc"
     : process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-      ? "https://www.kittr.gg/api/trpc"
+      ? "https://kittr.gg/api/trpc"
       : "http://localhost:3000/api/trpc"
 
 export const getUrl = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://www.kittr.gg"
+    ? "https://kittr.gg"
     : "http://localhost:3000"
