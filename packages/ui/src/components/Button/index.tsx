a23f4347fc2100f378d513d23bafad9bc3b5fc49
paulr093 <paulrichan@icloud.com>
2022-11-13 12:30:05 -0700

    Added UI for admin to add game.
    I think the logic is set up to add the game. Had to add a disabled prop to kittr button because TS was yelling at me.

--- packages/ui/src/components/Button/index.tsx
+++ packages/ui/src/components/Button/index.tsx
@@ -7,8 +7,10 @@ import React from "react"
 export interface Props extends MantineButtonProps {
 	children?: React.ReactNode
 	onClick: () => void
+	disabled?: boolean
 }
 
 export const Button: React.FC<Props> = (props) => {
 	return <MantineButton radius="lg" size="lg" {...props} />
 }
+
