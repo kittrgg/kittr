15c1646e4070051bd8bce57f1eeec625a66a7a8e
Anthony Shew <anthony@inthezone.dev>
2022-11-12 17:48:21 -0700

    Start backend route for adding game.

--- apps/web/src/server/routers/games/index.ts
+++ apps/web/src/server/routers/games/index.ts
@@ -1,7 +1,13 @@
 import { createRouter } from "@Server/createRouter"
 import { GamesController } from "@Server/controllers/games"
+import { GameModel, GenreModel, PlatformModel } from "@kittr/prisma/validator"
+import { z } from "zod"
 
 export const gamesRouter = createRouter()
 	.merge("getByUrlSafeName", GamesController.getGameByUrlSafeName)
 	.merge("getById", GamesController.getGameById)
 	.merge("list", GamesController.listGames)
+	.mutation("add", {
+		input: GameModel.omit({ id: true }).extend({ genres: z.array(GenreModel), platforms: z.array(PlatformModel) }),
+		resolve: () => "success"
+	})
