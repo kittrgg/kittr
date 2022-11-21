e87c3fb7450b3067745afe3b26baeca32c186a90
Anthony Shew <anthony@inthezone.dev>
2022-11-20 21:04:29 -0700

    Small things for Sentry.

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -4,7 +4,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
 	dsn: SENTRY_DSN,
-	environment: process.env.IS_DEV ? "development" : process.env.VERCEL_ENV,
+	environment: process.env.VERCEL_ENV,
 	tracesSampleRate: 1.0,
 	beforeSend: (event) => {
 		if (event.tags.isKittr) {
