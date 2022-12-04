f5774d52ff5f2d52965ada07a9d5fb4bc7f61209
anthonyshew <anthonyshew@users.noreply.github.com>
2022-12-04 08:10:50 +0000

    Apply Prettier fixes in Github Action

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
 
-import { Button } from '@kittr/ui';
+import { Button } from "@kittr/ui"
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
+	title: "Example/Button",
+	component: Button,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+	argTypes: {
+		backgroundColor: { control: "color" }
+	}
+} as ComponentMeta<typeof Button>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  children: <>The Button</>
-};
+	children: <>The Button</>
+}
