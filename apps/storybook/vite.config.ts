75ef2d24f4c513060898d7d8dcab0eebb8d1305a
Anthony Shew <anthony@inthezone.dev>
2022-12-04 01:09:25 -0700

    Feels decent...

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,14 +1,7 @@
-// eslint-disable-next-line spaced-comment
-/// <reference types="vite/client" />
-import reactPlugin from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [reactPlugin()]
-	// test: {
-	//   globals: true,
-	//   environment: "jsdom",
-	//   setupFiles: "./src/utils/setup.ts"
-	// }}
+  plugins: [react()]
 })
