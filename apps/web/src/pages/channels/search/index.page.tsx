4d2e65cfa261e9aba916ff62d702fb8deda67e0b
Paul Richan <75490402+paulr093@users.noreply.github.com>
2022-12-20 08:48:26 -0700

    Fix/styled components (#830)
    Co-authored-by: paulr093 <paulr093@users.noreply.github.com>
    Co-authored-by: Anthony Shew <anthony@inthezone.dev>

--- apps/web/src/pages/channels/search/index.page.tsx
+++ apps/web/src/pages/channels/search/index.page.tsx
@@ -1,13 +1,14 @@
-import AdPageWrapper, { H1 } from "@Components/layouts/AdPageWrapper"
+import AdPageWrapper from "@Components/layouts/AdPageWrapper"
 // import { ChannelSearch } from "@Components/shared"
 import ResponsiveBanner from "@Services/venatus/ResponsiveBanner"
+import { Title } from "@kittr/ui"
 import styled from "styled-components"
 
 const ChannelsSearch = () => (
 	<AdPageWrapper title="Search | kittr" description="Who ya got? | kittr">
 		<ResponsiveBanner />
 		{/* <ChannelSearch /> */}
-		<H1>CHANNELS</H1>
+		<Title preset="h1">CHANNELS</Title>
 		<Paragraph>Enter a search term.</Paragraph>
 	</AdPageWrapper>
 )
