dd95dc49887d244c3f73487c9f2cfda093a6fda4
Anthony Shew <anthony@inthezone.dev>
2022-12-28 21:37:06 -0700

    Bump ToS version.

--- apps/web/src/pages/terms-of-use.page.tsx
+++ apps/web/src/pages/terms-of-use.page.tsx
@@ -8,7 +8,7 @@ const Terms = () => {
 			<Styled.Container>
 				<h2>Website Terms of Use</h2>
 
-				<p>Version 1.4</p>
+				<p>Version 1.5</p>
 
 				<p>
 					The kittr website located at https://kittr.gg is a copyrighted work belonging to In the Zone Development.
