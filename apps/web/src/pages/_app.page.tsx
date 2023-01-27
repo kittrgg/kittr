9e6e40adbd9218f7b1cb9610fb351fada34ef0dc
Anthony Shew <anthony@inthezone.dev>
2023-01-26 19:38:08 -0700

    Finish it off.

--- apps/web/src/pages/_app.page.tsx
+++ apps/web/src/pages/_app.page.tsx
@@ -9,72 +9,46 @@ import { getTrpcUrl } from "@Utils/helpers/getUrl"
 import { Routes } from "@Utils/lookups/routes"
 import { MantineProvider } from "@kittr/ui"
 import { Global } from "@mantine/core"
-// import { useEffect } from "react"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { httpBatchLink } from "@trpc/client/links/httpBatchLink"
 import { loggerLink } from "@trpc/client/links/loggerLink"
 import { createTRPCNext } from "@trpc/next"
 import { useRouter } from "next/router"
-// import { useEffect } from "react"
 import { Provider } from "react-redux"
 import superjson from "superjson"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const AppWrap = ({ Component, pageProps }: { Component: React.FC; pageProps: Record<any, any> }) => (
-	<Provider store={store}>
-		<MyApp Component={Component} pageProps={pageProps} />
-	</Provider>
-)
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const MyApp = ({ Component, pageProps }: { Component: React.FC; pageProps: Record<any, any> }) => {
 	const router = useRouter()
-	// const dispatch = useDispatch()
-	// const isFallback = useSelector((state) => state.global.fallbackLoader)
-
-	// useEffect(() => {
-	// 	router.events.on("routeChangeStart", (url) => {
-	// 		if (url !== window.location.pathname) {
-	// 			// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-extra-semi
-	// 			;(window as any).routeTimeout = setTimeout(() => {
-	// 				dispatch(setFallbackLoader(true)), 500
-	// 			})
-	// 		}
-	// 	})
-
-	// 	router.events.on("routeChangeComplete", () => {
-	// 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// 		clearTimeout((window as any).routeTimeout)
-	// 		dispatch(setFallbackLoader(false))
-	// 	})
-	// }, [router.events, dispatch])
 
 	return (
-		<MantineProvider>
-			<Global
-				styles={() => ({
-					"body": {
-						// This is because the desktop footers for Venatus try to buy themselves space.
-						// We will handle this manually if we need to.
-						marginBottom: "0 !important"
-					},
-					// Make sure that Venatus ads are always a minimum of 320px wide.
-					'[data-ref="vm-preloader"]': {
-						minWidth: "320px !important"
-					}
-				})}
-			/>
+		<Provider store={store}>
+			<MantineProvider>
+				<Global
+					styles={() => ({
+						"body": {
+							// This is because the desktop footers for Venatus try to buy themselves space.
+							// We will handle this manually if we need to.
+							marginBottom: "0 !important"
+						},
+						// Make sure that Venatus ads are always a minimum of 320px wide.
+						'[data-ref="vm-preloader"]': {
+							minWidth: "320px !important"
+						}
+					})}
+				/>
 
-			{router.route === Routes.CHANNEL.GAME.createOverlayPath("[channel]", "[game]") ? (
-				<OverlayStyles />
-			) : (
-				<GlobalStyles />
-			)}
-			{/* {isFallback && <FallbackPage />}
+				{router.route === Routes.CHANNEL.GAME.createOverlayPath("[channel]", "[game]") ? (
+					<OverlayStyles />
+				) : (
+					<GlobalStyles />
+				)}
+				{/* {isFallback && <FallbackPage />}
 			{!isFallback && <Component {...pageProps} />} */}
-			<Component {...pageProps} />
-			<ReactQueryDevtools />
-		</MantineProvider>
+				<Component {...pageProps} />
+				<ReactQueryDevtools />
+			</MantineProvider>
+		</Provider>
 	)
 }
 
