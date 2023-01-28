fca9bec43b523bc1cb0caa47d89c3f0007baaf52
Anthony Shew <anthony@inthezone.dev>
2023-01-28 12:37:59 -0700

    Fix mocking.

--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -1,11 +1,10 @@
-import { prismaMock } from "./mock"
 import { PrismaClient } from "@prisma/client"
 
 export let prisma: PrismaClient
 
 if (typeof window === "undefined") {
 	if (process.env.NODE_ENV === "test") {
-		prisma = prismaMock
+		import(__dirname + "/mock.ts").then((mock) => (prisma = mock.prismaMock))
 	} else if (process.env.NODE_ENV === "production") {
 		prisma = new PrismaClient()
 	} else {
