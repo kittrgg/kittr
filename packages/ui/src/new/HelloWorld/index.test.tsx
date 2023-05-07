f12a7648ded09aef4ec4da6beb0d8f4566d0706d
Anthony Shew <anthony.shew@vercel.com>
2023-05-07 08:09:00 -0700

    Units are good.

--- packages/ui/src/new/HelloWorld/index.test.tsx
+++ packages/ui/src/new/HelloWorld/index.test.tsx
@@ -1,6 +1,6 @@
 import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { HelloWorld } from "./HelloWorld";
+import { HelloWorld } from ".";
 
 describe("<HelloWorld />", () => {
   test("Mounts", () => {
