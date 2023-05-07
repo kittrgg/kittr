839fc6715163e72ad4117508a6bed6ab6b847158
Jacob Sickels <33973325+JacobSickels@users.noreply.github.com>
2023-05-07 09:40:04 -0500

    Adds GameCard to UI components (#861)

--- apps/design-react/src/app/page.tsx
+++ apps/design-react/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { PlayerCard, Typography } from "@kittr/ui/new";
+import { GameCard, PlayerCard, Typography } from "@kittr/ui/new";
 import "@kittr/ui/styles.css";
 
 export default function Home() {
@@ -16,6 +16,51 @@ export default function Home() {
         isLive
         name="JoeWo"
       />
+
+      <div>
+        <GameCard
+          developer="Infinity Ward"
+          genres={["Battle Royale", "FPS"]}
+          imageProps={{ src: "https://placehold.co/160x256" }}
+          platforms={["PC", "PS4", "XBOX"]}
+          title="Warzone 2.0"
+        />
+
+        <GameCard
+          developer="Activision"
+          genres={["Battle Royale", "FPS"]}
+          imageProps={{ src: "https://placehold.co/160x256" }}
+          platforms={["PC", "PS4", "XBOX"]}
+          title="Warzone"
+        />
+
+        <GameCard
+          developer="Riot Games"
+          disabled
+          genres={["MOBA"]}
+          imageProps={{ src: "https://placehold.co/160x256" }}
+          platforms={["PC"]}
+          title="League of Legends"
+        />
+
+        <GameCard
+          developer="Battlestate Games"
+          disabled
+          genres={["FPS", "Survival"]}
+          imageProps={{ src: "https://placehold.co/160x256" }}
+          platforms={["PC"]}
+          title="Escape from Tarkov"
+        />
+
+        <GameCard
+          developer="Valve Corporation"
+          disabled
+          genres={["MOBA", "Survival"]}
+          imageProps={{ src: "https://placehold.co/160x256" }}
+          platforms={["PC", "PS4", "XBOX"]}
+          title="Dota 2"
+        />
+      </div>
 
       <Typography.H1>H1. Heading</Typography.H1>
       <Typography.H2>H2. Heading</Typography.H2>
--- packages/ui/src/new/index.ts
+++ packages/ui/src/new/index.ts
@@ -1,5 +1,7 @@
 import "../styles.css";
 
+export * from "./Badge";
+export * from "./GameCard";
 export * from "./HelloWorld";
 export * from "./PlayerCard";
 export * from "./Typography";
