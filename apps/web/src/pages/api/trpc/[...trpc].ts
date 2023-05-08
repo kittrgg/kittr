7a526d69c786fffcf785abf58d6a02e257ba821b
Anthony Shew <anthony.shew@vercel.com>
2023-05-07 21:19:41 -0700

    Maybe Sentry is bad?

--- apps/web/src/pages/api/trpc/[...trpc].ts
+++ apps/web/src/pages/api/trpc/[...trpc].ts
@@ -1,12 +1,11 @@
 import { appRouter, createContext } from "@kittr/trpc"
-import { withSentry } from "@sentry/nextjs"
 import * as trpcNext from "@trpc/server/adapters/next"
 
 // Export type definition of API
 export type AppRouter = typeof appRouter
 
 // Export API handler
-export default withSentry(
+export default
 	trpcNext.createNextApiHandler({
 		router: appRouter,
 		createContext,
@@ -14,4 +13,3 @@ export default withSentry(
 			enabled: true
 		}
 	})
-)
