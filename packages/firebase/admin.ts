d0c719c23b9aa733016fec420597f889de6ebb65
Anthony Shew <anthony.shew@vercel.com>
2023-05-08 21:52:19 -0700

    Remove some dumb console.logs.

--- packages/firebase/admin.ts
+++ packages/firebase/admin.ts
@@ -2,7 +2,6 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    console.log('yep, ran here YEP');
     admin.initializeApp({
       credential: admin.credential.cert({
         privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(
@@ -13,10 +12,8 @@ if (!admin.apps.length) {
         projectId: process.env.FIREBASE_PROJECT_ID,
       }),
     });
-    console.log('YOU INITTED');
   } catch (err) {
     console.error(err);
-    console.error('Something is wrong with your Firebase key.');
     throw new Error('Something is wrong with Firebase.');
   }
 }
