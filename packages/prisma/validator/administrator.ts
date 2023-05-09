3562fdae9aa23df10c672744ab6fee04d5ff016a
Anthony Shew <anthony.shew@vercel.com>
2023-05-08 20:54:14 -0700

    delete validators folder

--- packages/prisma/validator/administrator.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-import { AdminRole } from "@prisma/client"
-
-export const AdministratorModel = z.object({
-  id: z.string(),
-  firebaseUserId: z.string(),
-  role: z.nativeEnum(AdminRole),
-})
