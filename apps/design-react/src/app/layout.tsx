e4b49ac4d88376d1f41d4f57f6820a5aea49ae27
Anthony Shew <anthony.shew@vercel.com>
2023-05-12 23:39:37 -0700

    With my missed files.

--- apps/design-react/src/app/layout.tsx
+++ apps/design-react/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { cn } from '@kittr/ui/new';
 import { Sidebar } from './Sidebar';
 
 const inter = Inter({
