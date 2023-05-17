ce072edcdcedb51b8c7e397635ee60c670d314e9
Anthony Shew <anthony.shew@vercel.com>
2023-05-16 17:35:39 -0700

    A couple quick player card styles.

--- packages/ui/src/new/PlayerCard/index.tsx
+++ packages/ui/src/new/PlayerCard/index.tsx
@@ -18,28 +18,31 @@ export const PlayerCard = ({
   return (
     <button
       className={cn(
-        'relative m-4 w-[200px] rounded-xl bg-zinc-900 p-4 px-8 text-center shadow-md outline-none hover:cursor-pointer hover:bg-zinc-800 focus:shadow-slate-600 active:bg-zinc-700',
+        'relative m-4 w-[200px] rounded-xl bg-zinc-800 p-4 px-8 text-center shadow-md outline-none hover:cursor-pointer hover:bg-zinc-800 focus:shadow-slate-600 active:bg-zinc-700',
         className,
       )}
     >
-      <Avatar className="m-auto h-[100px] w-[100px]">
-        <AvatarFallback>...</AvatarFallback>
-        <AvatarImage alt={name} src={imageSrc} />
-      </Avatar>
-
-      {isLive && (
-        <div
-          className={cn(
-            'absolute bottom-16 left-1/2 -translate-x-1/2 rounded-md bg-red-600',
-          )}
-        >
-          <Span className={cn('px-2 py-0.5 tracking-widest')}>LIVE</Span>
-        </div>
-      )}
+      <div className="relative">
+        <Avatar className="m-auto h-[100px] w-[100px]">
+          <AvatarFallback>...</AvatarFallback>
+          <AvatarImage alt={name} src={imageSrc} />
+        </Avatar>
+        {isLive && (
+          <div
+            className={cn(
+              'absolute -bottom-2 left-1/2 -translate-x-1/2 rounded-md bg-red-600',
+            )}
+          >
+            <Span className={cn('px-2 py-0.5 text-sm tracking-widest')}>
+              LIVE
+            </Span>
+          </div>
+        )}
+      </div>
 
       <P
         className={cn(
-          'w-40 overflow-hidden truncate whitespace-nowrap text-xl font-bold tracking-widest',
+          'overflow-hidden truncate whitespace-nowrap pt-4 text-xl font-bold tracking-widest',
         )}
       >
         {name}
