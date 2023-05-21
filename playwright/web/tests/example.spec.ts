37c25ef2a957aed999e125cf42881c9f223bfeb8
Anthony Shew <anthony.shew@vercel.com>
2023-05-20 22:38:58 -0700

    Good to go?

--- playwright/web/tests/example.spec.ts
+++ playwright/web/tests/example.spec.ts
@@ -3,4 +3,32 @@ import { test, expect } from '@playwright/test';
 test('Renders home page', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle('Home | kittr');
+  page.getByText('27000+ kits');
+  page.getByText('JoeWo');
+});
+
+test('Renders games page', async ({ page }) => {
+  await page.goto('/games');
+  await expect(page).toHaveTitle('Games | kittr');
+  page.getByText('Warzone 2.0');
+});
+
+test('Renders channels page', async ({ page }) => {
+  await page.goto('/channels');
+  await expect(page).toHaveTitle('Channels - Page 1 | kittr');
+  page.getByText('HusKerrs');
+});
+
+test('Logs into dashboard and shows channels', async ({ page }) => {
+  if (!process.env.PLAYWRIGHT_USER_PASSWORD) {
+    throw new Error('No PLAYWRIGHT_USER_PASSWORD');
+  }
+
+  await page.goto('/dashboard');
+  await expect(page).toHaveTitle('Dashboard | kittr');
+  await page.click("[name='email']");
+  await page.keyboard.type('anthonyshew@gmail.com');
+  await page.click("[name='password']");
+  await page.keyboard.type(process.env.PLAYWRIGHT_USER_PASSWORD);
+  page.getByText('anthonyshew');
 });
