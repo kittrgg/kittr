ed12736e897e3dd8b4c7a5a98ceb04bc688470bf
Anthony Shew <anthony.shew@vercel.com>
2023-05-26 23:55:54 -0700

    Put Inter into App Router.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,18 @@
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className={`${inter.className}`} lang="en">
       <body>{children}</body>
     </html>
   );
