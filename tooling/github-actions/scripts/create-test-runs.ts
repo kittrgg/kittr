df5bee5c2befe6105b1b8e9511873b65bf6d1508
Anthony Shew <anthony.shew@vercel.com>
2023-05-27 15:26:27 -0700

    See how this goes.

---
+++ tooling/github-actions/scripts/create-test-runs.ts
@@ -0,0 +1,48 @@
+import { setFailed } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+interface Context {
+  eventName: 'deployment_status' | 'deployment';
+  ref: string;
+  payload: {
+    deployment_status: {
+      state: 'error' | 'failure' | 'success';
+      target_url: string;
+      environment: string;
+    };
+  };
+}
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+export function getOctokitClient(): ReturnType<typeof getOctokit> {
+  if (!GITHUB_TOKEN) {
+    const errorMessage = 'GITHUB_TOKEN is not defined';
+    setFailed(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  return getOctokit(GITHUB_TOKEN);
+}
+
+const getContext = () => context as unknown as Context;
+const getTargetUrl = () => getContext().payload.deployment_status.target_url;
+const getRef = () => getContext().ref;
+const getEnvironment = () =>
+  getContext().payload.deployment_status.environment.split(' ').slice(-1)[0];
+
+getOctokitClient()
+  .rest.actions.createWorkflowDispatch({
+    workflow_id: `Playwright - ${getEnvironment()}`,
+    ref: getRef(),
+    owner: 'kittrgg',
+    repo: 'kittr',
+    inputs: {
+      deployment_url: getTargetUrl(),
+    },
+  })
+  // eslint-disable-next-line no-console
+  .then((res) => console.log(res))
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+  .catch((err) => setFailed(err.message));
