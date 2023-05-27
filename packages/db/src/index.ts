d33385a0b71687c28658800548c5bcc69b38a459
Anthony Shew <anthony.shew@vercel.com>
2023-05-26 23:02:35 -0700

    Remove unnneeded pg dep.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,7 +2,7 @@ import { createClient } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 
 if (!process.env.POSTGRES_URL) {
-  throw new Error('No Postgres_URL in ');
+  throw new Error('No Postgres_URL given to Drizzle.');
 }
 
 const dbClient = createClient({ connectionString: process.env.POSTGRES_URL });
