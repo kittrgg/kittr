76fbcee43e1a4623b2c4c0cbb41a2f472a315e55
Anthony Shew <anthony.shew@vercel.com>
2023-05-26 23:17:00 -0700

    Wow, that was easy.

--- apps/web/src/app/app-future/developers/page.tsx
+++ apps/web/src/app/app-future/developers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <h1>Hello from the App Router!</h1>;
+import { db, game } from '@kittr/db';
+
+export default async function Page() {
+  const data = await db.select().from(game);
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,12 @@
-import { createClient } from '@vercel/postgres';
+import { createPool } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { schema } from '../drizzle/schema';
 
 if (!process.env.POSTGRES_URL) {
   throw new Error('No Postgres_URL given to Drizzle.');
 }
 
-const dbClient = createClient({ connectionString: process.env.POSTGRES_URL });
+const dbClient = createPool({ connectionString: process.env.POSTGRES_URL });
 
-export const db = drizzle(dbClient);
+export const db = drizzle(dbClient, { schema });
+export * from '../drizzle/schema';
