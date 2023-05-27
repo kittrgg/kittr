0885a6853441612b5f49f2e03d3cecae1610b9b0
Anthony Shew <anthony.shew@vercel.com>
2023-05-27 12:21:21 -0700

    Maybe?

--- tooling/github-actions/scripts/getDeploymentOutputs.ts
+++ tooling/github-actions/scripts/getDeploymentOutputs.ts
@@ -15,15 +15,12 @@ interface Context {
 const getContext = () => context as unknown as Context;
 
 try {
-  console.log(context);
   const deploymentUrl = getContext().payload.deployment_status.target_url;
-  console.log(deploymentUrl);
   setOutput('deployment_url', deploymentUrl);
 
   const environment = getContext()
     .payload.deployment_status.environment.split(' ')
     .slice(-1);
-  console.log(environment);
   setOutput('environment', environment);
 } catch (error: unknown) {
   // @ts-expect-error Not going to worry about this.
