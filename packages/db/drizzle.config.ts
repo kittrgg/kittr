d753b0d8306b7620de73ef18adfd6a433cae2275
Anthony Shew <anthony.shew@vercel.com>
2023-06-11 00:03:00 -0700

    Fix for formatting.

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,9 +4,10 @@ if (!process.env.POSTGRES_URL) {
   throw new Error('No POSTGRES_URL provided to Drizzle.');
 }
 
-// prettier-ignore
-export default {
-  schema: "./src/schema.ts",
-  out: "./drizzle",
-  connectionString: `${process.env.POSTGRES_URL}?sslmode=require`
-} satisfies Config;
+const config: Config = {
+  schema: './src/schema.ts',
+  out: './drizzle',
+  connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
+};
+
+export default config;
