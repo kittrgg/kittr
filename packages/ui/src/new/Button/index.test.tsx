8ceaea9395c95a1e2725e478953ddb42bf24c44b
Anthony Shew <anthony@inthezone.dev>
2023-06-11 11:26:45 -0700

    Simplify linters and formats. (#918)

--- packages/ui/src/new/Button/index.test.tsx
+++ packages/ui/src/new/Button/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Button } from '.';
 
 describe('<Button />', () => {
--- tooling/config-eslint/utils/rules.js
+++ tooling/config-eslint/utils/rules.js
@@ -4,5 +4,11 @@ module.exports = {
     '@typescript-eslint/sort-type-constituents': 'warn',
     'no-floating-decimal': 'warn',
     'unicorn/filename-case': 'off',
+    'turbo/no-undeclared-env-vars': [
+      'error',
+      {
+        allowList: ['PORT', 'CI', 'PLAYWRIGHT_BASE_URL'],
+      },
+    ],
   },
 };
