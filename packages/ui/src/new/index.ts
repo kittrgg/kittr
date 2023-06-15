fe88ed85e4e54dc960d60e72200e1465f4cb47c7
Anthony Shew <anthony.shew@vercel.com>
2023-06-15 16:58:49 -0700

    FAQ done.

--- packages/ui/src/new/index.ts
+++ packages/ui/src/new/index.ts
@@ -4,7 +4,12 @@ export * from './AppShell';
 export { Floaty } from './AppShell/Floaty';
 export { SidebarHeader } from './AppShell/sidebar/Header';
 export { Button, buttonVariants } from './Button';
-export { Accordion } from './Accordion';
+export {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './Accordion';
 export { SidebarSeparator } from './AppShell/sidebar/Separator';
 export { Loader } from './Loader';
 export { Avatar, AvatarFallback, AvatarImage } from './Avatar';
