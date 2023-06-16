b64df8a5005cf177b4b4c8f148db7a809969f997
Anthony Shew <anthony.shew@vercel.com>
2023-06-15 17:12:35 -0700

    Legal pages.

--- tooling/config-eslint/utils/rules.js
+++ tooling/config-eslint/utils/rules.js
@@ -3,6 +3,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/sort-type-constituents': 'warn',
     'no-floating-decimal': 'warn',
+    'react/no-unescaped-entities': 'off',
     'unicorn/filename-case': 'off',
     'turbo/no-undeclared-env-vars': [
       'error',
