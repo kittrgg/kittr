b90bed20a9179405e6180650b0bbfd5cf957b3b9
Anthony Shew <anthony.shew@vercel.com>
2023-06-18 09:32:22 -0700

    Use Inter in OGs.

--- apps/web/src/app/api/og/route.tsx
+++ apps/web/src/app/api/og/route.tsx
@@ -1,14 +1,18 @@
 import { ImageResponse } from 'next/server';
+import { interBold, interRegular } from '@/app/api/og/fonts';
 
-export const runtime = 'edge';
 const baseURL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `http://localhost:${process.env.PORT || 3000}`;
 
-// Now make it dynamic
-export function GET(request: Request) {
+export const runtime = 'edge';
+export async function GET(request: Request) {
   const title = new URL(request.url).searchParams.get('title') ?? 'get kittd.';
 
+  const [interBoldData, interRegularData] = await Promise.all([
+    interBold,
+    interRegular,
+  ]);
   return new ImageResponse(
     (
       <div tw="flex flex-row items-center justify-between w-full h-full text-6xl bg-zinc-900 text-white text-center">
@@ -18,23 +22,36 @@ export function GET(request: Request) {
           tw="absolute w-full h-full"
           width={192}
         />
-        <div tw="flex flex-col ml-40">
+        <div style={{ fontFamily: 'Inter Bold' }} tw="flex flex-col ml-40">
           <div tw="pb-16">{title}</div>
-          <div tw="text-4xl text-zinc-400">kittr</div>
+          <div
+            style={{ fontFamily: 'Inter Regular' }}
+            tw="text-4xl text-zinc-400"
+          >
+            kittr
+          </div>
         </div>
-
-        <div tw="flex flex-col mr-40">
-          <img
-            height={196}
-            src={`${baseURL}/img/logo-no-text.svg`}
-            width={192}
-          />
-        </div>
+        <img
+          height={196}
+          src={`${baseURL}/img/logo-no-text.svg`}
+          tw="absolute bottom-12 right-12"
+          width={192}
+        />
       </div>
     ),
     {
       width: 1200,
       height: 600,
+      fonts: [
+        {
+          name: 'Inter Bold',
+          data: interBoldData,
+        },
+        {
+          name: 'Inter Regular',
+          data: interRegularData,
+        },
+      ],
     },
   );
 }
