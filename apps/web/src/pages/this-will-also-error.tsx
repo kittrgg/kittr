d84df8acccf6574fe59bd90756d6fe36e655d4bf
Anthony Shew <anthony.shew@vercel.com>
2023-06-18 17:29:43 -0700

    Does it work when shipped?

---
+++ apps/web/src/pages/this-will-also-error.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function Page() {
+  const [thing, setThing] = useState<any>();
+
+  return (
+    <div>
+      {thing}
+      <button onClick={() => setThing({ throw: 'error' })}>Click me</button>
+    </div>
+  );
+}
+
+export default Page;
