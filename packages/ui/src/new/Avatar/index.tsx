d5a809c3163e1d6266d1d43bb5e996b0bf20ce04
Anthony Shew <anthony.shew@vercel.com>
2023-08-09 17:31:40 -0700

    Supense maybe?

--- packages/ui/src/new/Avatar/index.tsx
+++ packages/ui/src/new/Avatar/index.tsx
@@ -1,4 +1,5 @@
 import { download } from '@kittr/firebase/storage';
+import { Suspense } from 'react';
 import { AvatarShineLoader } from '../Loader';
 import { User } from '../../icons';
 import {
@@ -21,17 +22,19 @@ export const Avatar = async ({
   isLive,
 }: AvatarProps) => {
   return hasProfileImg ? (
-    <div className="relative">
-      <AvatarPrimitive>
-        <AvatarFallback>
-          <AvatarShineLoader />
-        </AvatarFallback>
-        <AvatarImage src={await download(id)} alt={username} />
-      </AvatarPrimitive>
-      {isLive ? (
-        <span className="absolute top-0 z-10 block w-2.5 h-2.5 animate-pulse text-white bg-red-600 rounded-full" />
-      ) : null}
-    </div>
+    <Suspense fallback={<AvatarShineLoader />}>
+      <div className="relative">
+        <AvatarPrimitive>
+          <AvatarFallback>
+            <AvatarShineLoader />
+          </AvatarFallback>
+          <AvatarImage src={await download(id)} alt={username} />
+        </AvatarPrimitive>
+        {isLive ? (
+          <span className="absolute top-0 z-10 block w-2.5 h-2.5 animate-pulse text-white bg-red-600 rounded-full" />
+        ) : null}
+      </div>
+    </Suspense>
   ) : (
     <div className="text-gray-500 flex justify-center items-center overflow-hidden border w-[40px] h-[40px] border-gray-500 rounded-full">
       <User strokeWidth={0.75} width={30} height={30} />
