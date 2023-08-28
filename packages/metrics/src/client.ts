5a0805e3f69ac3da20a53c705b4c1979d0ece7a0
Anthony Shew <anthony.shew@vercel.com>
2023-08-28 10:19:10 -0700

    Bump package.

--- packages/metrics/src/client.ts
+++ packages/metrics/src/client.ts
@@ -1,6 +1,6 @@
-import { Client } from '@axiomhq/axiom-node';
+import { Axiom } from '@axiomhq/js';
 
-export const client = new Client({
+export const client = new Axiom({
   token: process.env.AXIOM_PERSONAL_TOKEN,
   orgId: process.env.AXIOM_ORG_ID,
 });
